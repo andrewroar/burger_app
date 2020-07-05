dcd62f5c17b894183a6b82876b0a1a1b6a979be0
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-07-05 18:59:29 +0100

    adding JawSQL

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,30 @@
 const mysql = require("mysql");
 
-const dbOptions = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burger_db",
-};
+//const dbOptions = {
+//  host: "localhost",
+//  port: 3306,
+//  user: "root",
+//  password: "",
+//  database: "burger_db",
+//};
 
-const connection = mysql.createConnection(dbOptions);
+//const connection = mysql.createConnection(dbOptions);
 
-const onConnect = (err) => {
-  if (err) throw err;
-  console.log("Successfully connected to the Burger DB");
-};
+//const onConnect = (err) => {
+//  if (err) throw err;
+//  console.log("Successfully connected to the Burger DB");
+//};
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "hacktheplanet",
+    database: "burger_db",
+  });
+}
 
 connection.connect(onConnect);
 
